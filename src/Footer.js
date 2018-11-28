b058f554d540cba910b0ad297983f43da94b2e28
Ashlyn Hunt <aahunt92@gmail.com>
2018-11-27 19:55:32 -0600

    greet

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+<div id="footer">
+    <div class="container">
+        <h3>Contact Info</h3>
+        <ul>
+            <li id="middle">
+                <i class="fab fa-twitter"></i>
+                Twitter: <a href="https://twitter.com/ash_blackghoul">@ash_blackghoul</a>
+            </li>
+            <li>
+                <i class="fas fa-paper-plane"></i>
+                Email: <a href="mailto:aahunt92@gmail.com">aahunt92@gmail.com</a>
+            </li>
+            <li></li>
+        </ul>
+</div>                
+    </div>
