9c3ca3cb7f1998db684fd99928acbd5bcd40f5ee
Ashlyn Hunt <aahunt92@gmail.com>
2018-12-12 20:29:39 -0600

    nsvigate

--- src/Navigation.js
+++ src/Navigation.js
@@ -18,20 +18,10 @@ function buildLink(link){
 }
 
 export default function Navigation(state){
-    var links = state[state.active].links;
-    for(let i = 0; i < links.length; i++){
-       list+=
-         `<li>
-        <a href="#">${links[i]}
-        </li>
-        `;
-        console.log(list);
-    }
-
-return `
-<div id="navigation">
-    <ul class="container">
-        ${list}
+    return `
+    <div id="navigation">
+        <ul class="container">
+        ${state[state.active].links.map(buildLink).join('')}
     </ul>
 </div>
 `;
